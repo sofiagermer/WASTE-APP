ec54116b16e390438f9d82866d4e40673ffe10c0
sofiagermer <sofiagermer8@gmail.com>
2021-05-17 10:21:10 +0100

    MIGUEL EU JURO QUE TE MATO

--- cmake-build-debug/CMakeFiles/WASTE_APP.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/WASTE_APP.dir/DependInfo.cmake
@@ -4,16 +4,16 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/src/Graph/Graph.cpp" "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Graph/Graph.cpp.obj"
-  "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/src/UI/Menu.cpp" "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/UI/Menu.cpp.obj"
-  "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/src/Utils/MapPoint.cpp" "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Utils/MapPoint.cpp.obj"
-  "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/src/Waste-App/Car.cpp" "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Waste-App/Car.cpp.obj"
-  "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/src/Waste-App/Driver.cpp" "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Waste-App/Driver.cpp.obj"
-  "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/src/Waste-App/GarbageCollectionFacility.cpp" "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Waste-App/GarbageCollectionFacility.cpp.obj"
-  "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/src/Waste-App/House.cpp" "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Waste-App/House.cpp.obj"
-  "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/src/Waste-App/TrashContainer.cpp" "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Waste-App/TrashContainer.cpp.obj"
-  "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/src/Waste-App/User.cpp" "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Waste-App/User.cpp.obj"
-  "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/src/main.cpp" "C:/Users/migue/Desktop/FACULDADE/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/main.cpp.obj"
+  "/home/sofia/Desktop/WASTE-APP/src/Elements/Car.cpp" "/home/sofia/Desktop/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Elements/Car.cpp.o"
+  "/home/sofia/Desktop/WASTE-APP/src/Elements/Driver.cpp" "/home/sofia/Desktop/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Elements/Driver.cpp.o"
+  "/home/sofia/Desktop/WASTE-APP/src/Elements/GarbageCollectionFacility.cpp" "/home/sofia/Desktop/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Elements/GarbageCollectionFacility.cpp.o"
+  "/home/sofia/Desktop/WASTE-APP/src/Elements/House.cpp" "/home/sofia/Desktop/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Elements/House.cpp.o"
+  "/home/sofia/Desktop/WASTE-APP/src/Elements/MapPoint.cpp" "/home/sofia/Desktop/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Elements/MapPoint.cpp.o"
+  "/home/sofia/Desktop/WASTE-APP/src/Elements/TrashContainer.cpp" "/home/sofia/Desktop/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Elements/TrashContainer.cpp.o"
+  "/home/sofia/Desktop/WASTE-APP/src/Elements/User.cpp" "/home/sofia/Desktop/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Elements/User.cpp.o"
+  "/home/sofia/Desktop/WASTE-APP/src/Graph/Graph.cpp" "/home/sofia/Desktop/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/Graph/Graph.cpp.o"
+  "/home/sofia/Desktop/WASTE-APP/src/UI/Menu.cpp" "/home/sofia/Desktop/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/UI/Menu.cpp.o"
+  "/home/sofia/Desktop/WASTE-APP/src/main.cpp" "/home/sofia/Desktop/WASTE-APP/cmake-build-debug/CMakeFiles/WASTE_APP.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "/Graph/Graph.h"
+#include "Graph/Graph.h"
 
 
 using namespace std;
