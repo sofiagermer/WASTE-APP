9c53afdb78ed45dd72bef6c52ae50d7fd3255a9a
Miguel Lopes <lopesazevedom@gmail.com>
2021-05-17 11:28:24 +0100

    working ?

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 int main() {
-    Graph graph("Map/nodes_lat_lon_porto.txt","Map/edges_porto.txt","Map/t02_tags_porto.txt");
-    cout<<"done";
+    Graph graph("../Map/nodes_lat_lon_porto.txt","../Map/edges_porto.txt","../Map/t02_tags_porto.txt");
     return 0;
 }
